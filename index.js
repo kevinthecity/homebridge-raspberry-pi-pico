a14728951bd16e53cdbaedcaebbde7df61265c2e
Kevin Grant <kevingrant5@gmail.com>
2023-04-15 23:07:51 -0400

    Fix index

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-const { HomebridgePlatform } = require('homebridge-platform');
 const RaspberryPiPicoPlatform = require('./accessories/RaspberryPiPicoAccessory');
-
-class RaspberryPiPicoPlugin extends HomebridgePlatform {
-  constructor(log, config, api) {
-    super(log, config, api);
-
-    if (!api) {
-      log.warn('Homebridge API not available. Plugin not starting.');
-      return;
-    }
-
-    // Register the RaspberryPiPicoPlatform with Homebridge.
-    api.registerPlatform(RaspberryPiPicoPlatform.PLATFORM_NAME, RaspberryPiPicoPlatform);
-  }
-}
 
 module.exports = (api) => {
   // This method is called when the plugin is initialized.
 
   // For dynamic platform plugins, registerPlatform is called once for each instance of the platform
   // when it is accessed by Homebridge.
-  api.registerPlatform(RaspberryPiPicoPlatform.PLATFORM_NAME, RaspberryPiPicoPlugin);
+  api.registerPlatform(RaspberryPiPicoPlatform.PLATFORM_NAME, RaspberryPiPicoPlatform);
 };
